70537df4995b4525a0d4ac152441a09815c8232e
Benoit <47849605+cpnvbenoit@users.noreply.github.com>
2019-03-07 16:16:02 +0100

    Q. text aide
    Demande si je veux l'aide.

--- BattailleNaval/main.c
+++ BattailleNaval/main.c
@@ -1,6 +1,13 @@
+/*
+ * Benoit Pierrehumbert
+ * Bataille Navale
+ * 07.03.2019
+ * En cours: affichage aide
+ */
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Bonjours, voulez-vous afficher l'aide ?\n");
+
     return 0;
 }
